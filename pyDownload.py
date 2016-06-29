9b9be636d391755b769dc6dea50558e7257b847e
Dhruv Jauhar <dhruvjhr@gmail.com>
2016-06-29 11:42:35 +0530

    Updated pyDownload.py

--- pyDownload.py
+++ pyDownload.py
@@ -1,4 +1,9 @@
 import utlis.py
+import os
 dest = utils.check_os()
 def pyDownload(url, dest, hash = None, timeout =10):
-	pass
+	if os.path.exists(dest):
+		return
+	chunk = 1000 * 1000
+	temp_path = dest + '.pyd'
+	current size = os
