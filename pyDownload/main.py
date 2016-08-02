9a6e7adfbcc6d56db3bcc4d7f3f12fd5269a2e77
Dhruv Jauhar <dhruvjhr@gmail.com>
2016-08-02 22:31:17 +0530

    Added basic authentication.

--- pyDownload/main.py
+++ pyDownload/main.py
@@ -1,9 +1,16 @@
 #-*- coding: utf-8 -*-
 """
 Usage:
-	pyd <url> [--hash=<h>] [--auth <u> <p>] [--timout <t>] [--dest <d>]
+	pyd <url> [--hash <value>] [--auth <u> <p] [--timout <t>] [--dest <d>]
 
     Examples:
+    pyd https://www.python.org/static/img/python-logo.png  : Download's the python logo from their homepage
+    pyd --dest cur : Download's to current directory instead of default downloads folder
+    pyd --dest set : Let's you decide the download 
+    pyd https://www.example.com/... --auth username password : When authentication is required
+    pyd https://www.example.com/... --auth username : Takes password after the command without being displayed on screen (like in terminals)
+    pyd https://www.example.com/... --hash
+
 
     Options:
     -h,--help
