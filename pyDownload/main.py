8f92c8942f353414ca9f05096faf6c1542127538
Dhruv Jauhar <dhruvjhr@gmail.com>
2017-01-24 04:59:13 -0500

    broke the CLI, move to click from docopt

--- pyDownload/main.py
+++ pyDownload/main.py
@@ -1,26 +1,23 @@
 #-*- coding: utf-8 -*-
 """
 Usage:
-	pyd <url> [--hash <value>] [--auth <u> <p] [--timout <t>] [--dest <d>] [--version]
+	pyd <url> [--hash <value>] [--auth <u> <p] [--version]
 
     Examples:
     pyd https://www.python.org/static/img/python-logo.png  : Download's the python logo from their homepage
-    pyd --dest cur : Download's to current directory instead of default downloads folder
-    pyd --dest set : Let's you decide the download 
     pyd https://www.example.com/... --auth username password : When authentication is required
     pyd https://www.example.com/... --auth username : Takes password after the command without being displayed on screen (like in terminals)
-    pyd https://www.example.com/... --auth uname --timeout --hash = 1234 : We can aslo club commands
-
 
     Options:
     -h,--help
 """
 from docopt import docopt
-import getpass,pyDownload
+import getpass
+import pyDownload
 
 def start():
 	arguments = docopt(__doc__)
-	p = getpass.getpass()
-	print(arguments)
-	print("Olle!")
-	
+	pyDownload.main(arguments[0], arguments[1])
+
+if __name__ == '__main__':
+        start()	
