e51690093b166975e37a42db3fa6f6b96f5fba14
Dhruv Jauhar <dhruvjhr@gmail.com>
2017-01-24 04:57:33 -0500

    added download acceleration. broke authentication and cli

--- pyDownload/pyDownload.py
+++ pyDownload/pyDownload.py
@@ -1,133 +1,123 @@
 import os
+import requests
+import shutil
 import hashlib
-import sys
-#import getpass
+from threading import Thread
 import urllib.parse
-from mimetypes import guess_extension
-try:
-    import urllib.request as urllib2
-except ImportError:
-    import urllib2
 
-def get_file_name(url, flag = 1):
+def download_file(url, start, end, part, file_name):
+    local_filename = file_name+".part"+str(part)
+    try:
+    	start = os.path.getsize(local_filename)
+    except OSError:
+    	pass
+    headers = {'Range': 'bytes={}-{}'.format(start, end)}
+    r = requests.get(url, headers=headers, stream=True)
+    with open(local_filename, 'ab') as f:
+        shutil.copyfileobj(r.raw, f)
+	return local_filename
+
+def simple_download(url, file_name):
+    try:
+    	start = os.path.getsize(local_filename)
+    except OSError:
+    	start = 0
+    headers = {'Range': 'bytes={}-'.format(start)}
+    r = requests.get(url, headers=headers, stream=True)
+    with open(local_filename, 'ab') as f:
+        shutil.copyfileobj(r.raw, f)
+	return local_filename
+
+def get_file_name(url):
 	# Checks if url has query variables.
 	# If so, it tries to find the name of the file from it.
 	file_name = None
-	if flag == 1:
-		parsed = urllib.parse.urlparse(url)
-		params = urllib.parse.parse_qsl(parsed.query)
-		for x,y in params:
-			if x == 'name' or x =='Name' :
-				file_name = y
-
-		if file_name == None:
-			name = url.split('/')[-1]
-			if name == "":
-				name = url.split('/')[-2]
-
-		if len(name) > 50:
-			name = "untitled"
-	# flag = 1 for setting the name automatically, flag = -1 for user to set the name
-	elif flag == -1:
-		name = input('File Name:\n>>>')
+	queries = urlparse.urlparse(url)
+	params = dict(urlparse.parse_qsl(queries.query))
+	if 'name' in params:
+		file_name = params['name']
+	if 'Name' in params:
+		file_name = params['Name']
+	if file_name == None:
+		name = url.split('/')[-1]
+		# in case url had / at the end
+		if name == "":
+			name = url.split('/')[-2]
+	# assumes if file name > 50 it is not the proper name
+	if len(name) > 50:
+		name = "untitled"
 	return name
 
-def set_default_paths(flag = 1):
-	"""
-	Used to set the deafult download location and the temporary file path
-	based on the enviornment
-	"""
-	OS = sys.platform
-	uname = os.getlogin() # Instead of longer pwd.getpwuid(os.getuid())[0]
-	default_dest = None
-	filename = get_file_name(url)
+def merge(file_name):
+	tempfiles = []
+	for i in range(1,6):
+		tempfiles.append(file_name+".part"+str(i))
+	with open(temp_file, "w") as fo:
+	     for tempfile in tempfiles:
+	          with open(tempfile,'r') as fi: fo.write(fi.read())
 
-	if flag == 1:
-		switch = {
-		'linux2': '/home/{}/Downloads/'.format(uname)+filename,
-		'win32': 'C:\\Users\\{}\\Downloads\\'.format(uname)+filename,
-		'darwin': 'C/Users/{}/Downloads/'.format(uname)+filename
-		}
-		default_dest = switch[OS]
+def call_threads(url, size, file_name):
+	# change thread calls to lambda function later
+	thread1 = Thread(target=download_file, (url, 0, (size/5), 1), file_name)
+	thread2 = Thread(target=download_file, (url, (size/5) + 1, (size/5)*2, 2), file_name)
+	thread3 = Thread(target=download_file, (url, ((size/5)*2) + 1, (size/5)*3, 3), file_name)
+	thread4 = Thread(target=download_file, (url, ((size/5)*3) + 1, (size/5)*4, 4), file_name)
+	thread5 = Thread(target=download_file, (url, ((size/5)*4) + 1, size, 5), file_name)
+	t = [thread1, thread2, thread3, thread4, thread5]
+	for x in t:
+		x.start()
+	for x in t:
+		x.join()
+	merge(file_name)
+	print("File Successfully Downloaded")
 
-	if (flag == 2 or default_dest == None):
-		default_dest = os.getcwd() + '/'
 
-	if flag == 3:
-		default_dest = input(">") + '/'
-	return default_dest
-
-def hash_check(file_dest, hash_value = None):
+def hash_check(hash_value, file_name):
 	# Assumes you are using MD5 or SHA-256
-	if hash_value == None:
+	if hash_value == 0:
 		print('Error: No hash value provided.')
 		return -1
 
-	result = None
-	if len(hash_value) == 32:
-		m = hashlib.md5()
+		result = None
+	# Assumes if hashsum length is 32 it is md5
+	if len(str(hash_value)) == 32:
+		BLOCKSIZE = 65536
+		hasher = hashlib.md5()
+		with open(file_name, 'rb') as afile:
+			buf = afile.read(BLOCKSIZE)
+			while len(buf) > 0:
+				hasher.update(buf)
+				buf = afile.read(BLOCKSIZE)
+    	return hasher.hexdigest() == hash_value
 
-		with open(default_dest, 'rb') as f:
-			while True:
-				chunk = f.read(1024 * 1024)
-				if not chunk:
-					break
-				m.update(chunk)
-		result = m.hexadigest() == hash_value
+	# Assumes if hashsum length is 64 it is sha256
+	if len(str(hash_value)) == 32:
+		BLOCKSIZE = 65536
+		hasher = hashlib.shaa256()
+		with open(file_name, 'rb') as afile:
+			buf = afile.read(BLOCKSIZE)
+			while len(buf) > 0:
+				hasher.update(buf)
+				buf = afile.read(BLOCKSIZE)
+    	return hasher.hexdigest() == hash_value
 
-	if len(hash_value) == 64:
-		m = hashlib.sha256()
-
-		with open(default_dest, 'rb') as f:
-			while True:
-				chunk = f.read(1024 * 1024)
-				if not chunk:
-					break
-				m.update(chunk)
-		result = (m.hexadigest() == hash_value)
-
-
-
-
-
-
-def pyDownload(url, dest = 1, hash = None, timeout =10):
-	
-	chunk = 1000 * 1000
-	extension = guess_extension(urllib2.urlopen(url).info().get('Content-Type', -1).split()[0].rstrip(";"))
-	temp_path = set_default_paths(dest) + extension
-	if os.path.exists(temp_path):
+def main(url, hash_value):
+	r = requests.head(url)
+	if r.status_code != 200:
+		print("Recieved Error Code: " + r.status_code)
 		return
-	temp_path += '.pyd'
-
-	if os.path.exists(temp_path):
-		open_size = os.path.getsize(temp_path)
-	else:
-		open_size = 0
-
-	file_size = -1
+	file_name = get_file_name(url)
 	try:
-		file_size = int(urllib2.urlopen(url).info().get('Content-Length', -1))
-		while open_size < file_size:
-			current_byte = open_size + chunk \
-				if open_size + chunk < file_size \
-				else file_size
-			req = urllib2.Request(url)
-			req.headers['Range'] = 'bytes={}={}'.format(open_size,current_byte)
-			data_chunk = urllib2.urlopen(req, timeout=timeout).read()
-	
-			with open(temp_path, 'ab') as x:
-				x.write(data_chunk)
-			open_size = current_byte + 1
-	except ImportError as e:
-		print('IO Error - {}'.format(e))
-	finally:
-		if file_size == os.path.getsize(temp_path):
-			os.rename(temp_path,temp_path[:-4])
+		size = r.headers['content-length']
+		call_threads(url, size, file_name)
+	except KeyError:
+		simple_download(url, file_name)
+	if hash != '':
+		printhash_check(hash_value, file_name)
 
-url = 'https://www.safaribooksonline.com/library/cover/9780470532874/240h/'
-#url = 'http://blog.udacity.com/2014/05/how-to-bulk-rename-files-with-python.html'
-
-
-download = pyDownload(url)
+#url = 'https://dl.google.com/android/repository/tools_r25.2.3-windows.zip'
+#url2 = 'https://www.fosshub.com/qBittorrent.html/qbittorrent_3.3.10_setup.exe'
+#url3 = 'http://williampatino.com/2015/wp-content/uploads/2016/12/William_Patino_Photography_NewZealand_Norwest_Lakes-copy.jpg'
+#x = download_file(url, 0, 153393413, 1)
+#y = download_file(url, 1025, 153393413+1, 2)
 
